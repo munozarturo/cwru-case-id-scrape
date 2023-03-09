c32a50c1163f82bd464bf4603dc0595795237190
munozarturo <munoz.arturoroman@gmail.com>
2023-03-09 15:52:55 -0500

    restructured brute force

--- brute_force.py
+++ brute_force.py
@@ -1,3 +1,10 @@
+from itertools import product
+import re
+from urllib.request import urlopen
+from cwru.cwru import generate_query_url
+from log import Logger
+from scraper import SequentialScraper
+
 """
 This module mainly tries to collect all student emails from the university
 by using every possible 3 letter combination of the alphabet and a wildcard to
@@ -19,112 +26,17 @@ General Notes
 
 """
 
-from itertools import product
-from pathlib import Path
-from urllib.request import urlopen
+logger: Logger = Logger(print_=True, file_=True, file_path="log.txt")
 
-from cwru import generate_query_url
+urls: list[str] = [generate_query_url(seach_text=query, category="student") for query in
+    [f"{''.join(c)}*" for c in product("abcdefghijklmnopqrstuvwxyz", repeat=3)]]
 
-from log import Logger
-from validate import validate
+scraper: SequentialScraper = SequentialScraper(
+    url=urls,
+    request_func=lambda url: urlopen(url).read().decode("utf-8"),
+    scrape_func=lambda html: list(set(re.findall('[\w\.-]+@case.edu+', html))),
+)
 
-import re
-
-class Error(Exception):
-    """Base class for exceptions in this module."""
-    pass
-
-class RequestError(Error):
-    """Base class for request exceptions."""
-    pass
-
-def scrape_info_regex(url: str) -> list[str]:
-    """
-    Scrape email addresses from a Case Western Reserve University directory query url.
-
-    Args:
-        url (str): URL to scrape.
-
-    Returns:
-        list[str]: list of @case.edu email addresses.
-    """
-    
-    validate(url, str)
-    
-    # request content
-    request = urlopen(url)
-    
-    # check for errors
-    if request.getcode() == 500:
-        raise RequestError("Server Error")
-    
-    html: str = request.read().decode("utf-8")
-    # find all email addresses that match the pattern email@case.edu
-    matches: list[str] = list(re.findall('[\w\.-]+@case.edu+', html))
-    
-    # convert to set and then back to list to remove duplicates
-    return list(set(matches))
-
-def brute_force() -> None:
-  logger: Logger = Logger(print_=True, file_=True, file_path=Path("log.txt"))
-  
-  # output file
-  output_to: Path = Path("results.txt")
-  dump_on_exit: Path = Path("dump.txt")
-
-  # List of possible pseudo given names.
-  # Generated with the cartesian product of the lowercase alphabet ^ 3.
-  given_names: list[str] = [f"{''.join(c)}*" for c in product("abcdefghijklmnopqrstuvwxyz", repeat=3)]
-
-  # quick check that every possible combination has been generated
-  assert len(given_names) == 26**3
-
-  try:
-      # scrape all query urls
-      i: int = 0
-      total_scraped: int = 0
-      while given_names:
-          # get query url
-          current_name: str = given_names[0]
-          query_url: str = generate_query_url(seach_text=given_names.pop(0), category="student")
-
-          # print progress
-          logger.log(f"Scraping {current_name}...")
-          logger.log(f"{i+1}/{len(given_names)} ({round((i+1)/len(given_names)*100, 2)}%) {len(given_names)} remaining...", indent_level=1)
-          
-          # get results
-          try:
-              # scrape
-              results: list[str] = scrape_info_regex(query_url)
-              
-              # update total scraped
-              total_scraped += len(results)
-              
-              # print results
-              logger.log(f"Found {len(results)} results.", indent_level=1, source=current_name)
-              logger.log(f"Total scraped: {total_scraped}", indent_level=1)
-
-              # write results to file
-              for result in results:
-                  with open(output_to, "a") as file:
-                      file.write(result + "\n")
-          except RequestError as e:
-              # if there is an error, add the query url to the list
-              logger.log(f"Error scraping {current_name}...", indent_level=1, source=current_name)
-              logger.log(f"Added to query_urls list.", indent_level=2, source=current_name)
-              
-              given_names.append(current_name)
-                  
-          i += 1
-  except Exception as e:
-      logger.log(f"Error: {e}")
-  except KeyboardInterrupt:
-      logger.log("Exiting...")
-      logger.log(f"Results saved to '{output_to}'.", indent_level=1)
-      logger.log(f"Dumping remaining query urls to '{dump_on_exit}'.", indent_level=1)
-      
-      with open(dump_on_exit, "w") as file:
-          for name in given_names:
-              file.write(name + "\n")
-      
-      exit(0)
+results: list[list[str]] = scraper.run(
+    callback=lambda i, url: Logger.log(f"Scraping {i} of {len(urls)}: {url}")
+)
