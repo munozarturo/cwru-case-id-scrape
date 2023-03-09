2a8ef01fa21cefed2c9277bf7647be8b23e78444
munozarturo <munoz.arturoroman@gmail.com>
2023-03-09 14:05:22 -0500

    changed log to class

--- log/log.py
+++ log/log.py
@@ -1,48 +1,95 @@
+from pathlib import Path
 from types import NoneType
-from typing import Union
+from typing import Any
 from validate.vval import validate
 
-LOG_NUM: int = 0
-PRINT: bool = True
-LOG: bool = True
+class Logger:
+    @property
+    def log_num(self) -> int:
+        return self.__log_num
+    
+    @log_num.setter
+    def log_num(self, log_num: int) -> None:
+        raise AttributeError("Cannot set log_num.")
+    
+    @log_num.deleter
+    def log_num(self) -> None:
+        raise AttributeError("Cannot delete log_num.")
+    
+    @property
+    def outpath(self) -> Path | NoneType:
+        return self.__outpath
+    
+    @outpath.setter
+    def outpath(self, outpath: str | Path | NoneType | Any) -> None:
+        try:
+            self.__outpath = outpath if isinstance(outpath, Path) else Path(outpath)
+        except :
+            raise TypeError("`outpath` must be pathlike.")
+        
+    @outpath.deleter
+    def outpath(self) -> None:
+        self.__outpath = None
+    
+    @property
+    def print(self) -> bool:
+        return self.__print
+    
+    @print.setter
+    def print(self, print_: bool) -> None:
+        validate(print_, bool)
+        
+        self.__print = print_
+        
+    @print.deleter
+    def print(self) -> None:
+        raise AttributeError("Cannot delete print.")
+    
+    @property
+    def do_log(self) -> bool:
+        return self.__do_log
+    
+    @do_log.setter
+    def do_log(self, do_log: bool) -> None:
+        validate(do_log, bool)
+        
+        self.__do_log = do_log
+    
+    def __init__(self, outpath: str | Path | NoneType | Any, print_: bool = True, do_log: bool = True) -> None:
+        self.__log_num: int = 0
+        self.__print: bool = True
+        self.__do_log: bool = True
+        self.__outpath: Path | NoneType = None
+        
+        self.print = print_
+        self.do_log = do_log
+        self.outpath = outpath
+        
+    def log(self, msg: str, indent_level: int = 0, source: str | NoneType = None) -> None:
+        """
+        Print `msg` to console and write it to `log.txt`.
 
-def set_print(print_: bool) -> None:
-    global PRINT
-    PRINT = print_
-    
-def set_log(log_: bool) -> None:
-    global LOG
-    LOG = log_
-
-def log(msg: str, indent_level: int = 0, source: str | NoneType = None) -> None:
-    """
-    Print `msg` to console and write it to `log.txt`.
-
-    Args:
-        msg (str): Message to be logged.
-        indent_level (int, optional): Indentation level. Defaults to 0.
-    """
-    
-    validate(msg, str)
-    validate(indent_level, int)
-    validate(source, (str, NoneType))
-    
-    global LOG_NUM
-    
-    indent: str = "    " * indent_level
-    
-    if source is not None:
-        _msg: str = f"{str(LOG_NUM).rjust(7)}  {indent} {source}: {msg}"
-    else:
-        _msg: str = f"{str(LOG_NUM).rjust(7)}  {indent} {msg}"
-    
-    global PRINT
-    if PRINT:
-        print(_msg)
-    
-    global LOG
-    if LOG:
-        with open("log.txt", "a") as file:
-            file.write(_msg + "\n")
-    
-    LOG_NUM += 1
+        Args:
+            msg (str): Message to be logged.
+            indent_level (int, optional): Indentation level. Defaults to 0.
+        """
+        
+        validate(msg, str)
+        validate(indent_level, int)
+        validate(source, (str, NoneType))
+        
+        indent: str = "    " * indent_level
+        
+        if source is not None:
+            _msg: str = f"{str(self.log_num).rjust(7)}  {indent} {source}: {msg}"
+        else:
+            _msg: str = f"{str(self.log_num).rjust(7)}  {indent} {msg}"
+        
+        if self.print:
+            print(_msg)
+        
+        if self.do_log:
+            with open(self.outpath, "a") as file:
+                file.write(_msg + "\n")
+        
+        self.__log_num += 1
--- main.py
+++ main.py
@@ -1,1 +1,2 @@
 from approaches import auth_request, brute_force
+
