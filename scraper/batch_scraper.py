016c9bd748787ea04d84c6e4bfc3b1a6d6a20663
munozarturo <munoz.arturoroman@gmail.com>
2023-03-09 15:22:44 -0500

    added run() to batch scraper

--- scraper/batch_scraper.py
+++ scraper/batch_scraper.py
@@ -1,18 +1,53 @@
-
-
 from typing import Any, Callable, Iterable
 from scraper.scraper import Scraper
+from validate import validate
 
 
 class BatchScraper(Scraper):
-    def __init__(self, url: str | Iterable[str], request_func: Callable[[str], str], scrape_func: Callable[[str], str]) -> None:
+    def __init__(
+        self,
+        url: str | Iterable[str],
+        request_func: Callable[[str], str],
+        scrape_func: Callable[[str], str],
+    ) -> None:
+        """
+        Batch scraper.
+
+        Args:
+            url (str | Iterable[str]): url or urls to scrape.
+            request_func (Callable[[str], str]): function to request html from a url. Expects single str argument and returns str.
+            scrape_func (Callable[[str], str]): function to scrape html. Expects single str argument and returns Any.
+        """
+
         super().__init__(url, request_func, scrape_func)
-        
-    def run(self) -> list[Any]:
+
+    def run(self, callback: Callable[[int, str], Any] = None) -> list[Any]:
+        """
+        Run the batch scraper.
+
+        Args:
+            callback (Callable[[int, str], Any], optional): Called at the start of every sequential parse iteration. 
+                It is called with the current url iteration number and the url. Defaults to None.
+
+        Returns:
+            list[Any]: List of results.
+        """
+
+        # validate that callback is a callable
+        validate(callback, Callable)
+
+        # create a list to store the results
         results: list[Any] = []
-        
-        for url in self.urls:
+
+        # loop through the urls
+        for i, url in enumerate(self.urls):
+            # if a callback is provided, call it
+            if callback is not None:
+                callback(i, url)
+
+            # request the url and scrape the response
             response = self.request_func(url)
             results.append(self.scrape_func(response))
-            
+
+        # return the results
         return results
--- scraper/scraper.py
+++ scraper/scraper.py
@@ -1,64 +1,75 @@
 from validate import validate, validate_iterable, validate_option
 from typing import Any, Callable, Iterable
 
+
 class Scraper:
     @property
     def urls(self) -> Iterable[str]:
         return self.__urls
-    
+
     @urls.setter
     def urls(self, urls: str | Iterable[str]) -> None:
         if isinstance(urls, str):
             self.__urls = [urls]
         else:
             validate_iterable(urls, str)
-            
+
             self.__urls = urls
-        
+
     @urls.deleter
     def urls(self) -> None:
         self.__urls = []
-        
+
     @property
     def request_func(self) -> Callable[[str], str]:
         return self.__request_func
-    
+
     @request_func.setter
     def request_func(self, request_func: Callable[[str], str]) -> None:
         validate(request_func, Callable)
-        
+
         self.__request_func = request_func
-        
+
     @request_func.deleter
     def request_func(self) -> None:
         raise AttributeError("Cannot delete `request_func`.")
-    
+
     @property
     def scrape_func(self) -> Callable[[str], str]:
         return self.__scraper_func
-    
+
     @scrape_func.setter
     def scrape_func(self, scrape_func: Callable[[str], str]) -> None:
         validate(scrape_func, Callable)
-        
+
         self.__scraper_func = scrape_func
-        
+
     @scrape_func.deleter
     def scrape_func(self) -> None:
         raise AttributeError("Cannot delete `scrape_func`.")
-    
-    def __init__(self, 
-                 url: str | Iterable[str], 
-                 request_func: Callable[[str], str], 
-                 scrape_func: Callable[[str], str]) -> None:
-        
+
+    def __init__(
+        self,
+        url: str | Iterable[str],
+        request_func: Callable[[str], str],
+        scrape_func: Callable[[str], str],
+    ) -> None:
+        """
+        Scraper base class.
+
+        Args:
+            url (str | Iterable[str]): url or iterable of urls to scrape.
+            request_func (Callable[[str], str]): function to request url.
+            scrape_func (Callable[[str], str]): function to scrape url contents.
+        """
+
         self.__urls: Iterable[str] = []
         self.__request_func: Callable[[str], str] = None
         self.__scraper_func: Callable[[str], Any] = None
-        
+
         self.urls = url
         self.request_func = request_func
         self.scrape_func = scrape_func
-        
+
     def run(self) -> Any:
         raise NotImplementedError("`run()` must be implemented.")
